1d8a0062f568d26aa2c3f6c300172bc9b5aa99d8
AndreK01 <andre.kohler01@googlemail.com>
2024-04-21 22:01:35 +0200

    interim commit

--- app_analyzer_comfort.js
+++ app_analyzer_comfort.js
@@ -1,51 +1,256 @@
 //****************************************
 // some global variables
 //****************************************
-
 var mySerieItems = [];
 var source = "";
 var modes = "";
 var modeslist = "";
 var myExposures = ['area','areaspline','areastack','areastair','column','columnstack','line','linestack','spline','stair'];
 var ExposureList = "";
+var yAxisCount = 0;
+
+// Objects to store all the informations
+var myYAxis = {};
+var myTimeSettings = {};
+var myItems = {};
+var myChartOptions = {};
+var myStackings = {};
+var myStacks = {};
+
+
+// define default Time-Settings
+myDate= new Date()
+myTimeSettings = {
+                    "ItemType"  : "Database",
+                    "StartType" : "1",
+                    "StartDate" : myDate.toJSON().slice(0, 4) +'-01-01',
+                    "StartTime" : "00:00",
+                    "EndType"   : "1",
+                    "EndDate"   : myDate.toJSON().slice(0, 10),
+                    "EndTime"   : myDate.toJSON().slice(11, 16),
+                    "Duratin"   : "1d"
+                  }
+// define first Axis
+newAxisID="0001"
+myYAxis[newAxisID] = {}
+myYAxis[newAxisID]['no']= yAxisCount
+myYAxis[newAxisID]['apYmin']     = ""
+myYAxis[newAxisID]['apYmax']     = ""
+myYAxis[newAxisID]['apYpos']     = "0"
+myYAxis[newAxisID]['apYtype']    = "linear"
+myYAxis[newAxisID]['apYunit']    = ""
+// define First Item
+newItemID="0001"
+myItems[newItemID] = {}
+myItems[newItemID]['apItem'] = ""
+myItems[newItemID]['apMode'] = "avg"
+myItems[newItemID]['apCount'] = "100"
+myItems[newItemID]['apExposure'] = ""
+myItems[newItemID]['apColor'] = ""
+myItems[newItemID]['apAssign'] = newAxisID
+myItems[newItemID]['stack'] = ""
+
+
+
+var yAxisHeader = '<table style="width:97%;">\
+                    <tr>\
+                      <th style = "width:5%; text-align:center;">No.</th>\
+                      <th style = "width:14%; text-align:center;">Min-Value</th>\
+                      <th style = "width:14%; text-align:center;">Max-Value</th>\
+                      <th style = "width:14%; text-align:center;">Axis-Position</th>\
+                      <th style = "width:14%; text-align:center;min-width:100px;">Axis scale type</th>\
+                      <th style = "width:14%; text-align:center;">Axis unit</th>\
+                      <th style = "width:14%; text-align:center;">Function</th>\
+                    </tr>'
+
+var yAxisHtml = '<tr>\
+        <td style="text-align:center">{NO}</td>\
+        <td><input id="apYmin-{id}" onchange="changedYAxis(this)" ></td>\
+        <td><input id="apYmax-{id}" onchange="changedYAxis(this)"></td>\
+        <td><select id="apYpos-{id}" onchange="changedYAxis(this)" data-native-menu="false" style="font-size:0.8em;"><option value="0">left</option><option value="1">right</option></select>\</td>\
+        <td><select id="apYtype-{id}" onchange="changedYAxis(this)" data-native-menu="false"><option value="linear">linear</option><option value="logarithmic">logarithm</option><option value="boolean">bool</option></select></td>\
+        <td>\<input id="apYunit-{id}" onchange="changedYAxis(this)">\</td>\
+        <td>\
+          <div class="tooltip">\
+            <button id="del_Y_Axis-{id}" class="FctButton ui-mini ui-btn-inline" name="btn-delete-y-axis" type="button"  onclick="deleteYAxis(this)"><img src="icons/ws/jquery_delete.svg"></button>\
+            <span class="tooltiptext">remove Axis</span>\
+          </div>\
+        </td>\
+      </tr>';
+
+var itemHtml ='<div id="itemsetting{{source}}" class="itemsetting" data-role="collapsible" data-collapsed="true" data-theme="c" data-content-theme="a" style="width:98%;">\
+			<h3 style="display:inline"> *** NEW Item ***</h3>\
+				<table style="width:100%;">\
+					<tr>\
+					  <td style = "width:10%;">Item</td>\
+					  <td style = "width:50%;"><select id="apItem{{source}}" data-native-menu="false" style="font-size:0.8em;width:100%;" onchange="selectChanged(this)"> </select></td>\
+					  <td style = "width:15%;text-align:center;">DB-Mode</td>\
+					  <td style = "width:25%;" ><select id="apMode-{{source}}" data-native-menu="false" style="font-size:0.8em;" onchange="itemChange(this)"></select></td>\
+					  </tr>\
+        </table>\
+				<table>\
+          <tr>\
+					  <td style = "width:15%;">Count</td>\
+					  <td style = "width:10%;"><input id="apCount{{source}}" value="100" onchange=""itemChange(this)/></td>\
+					  <td style = "width:20%;text-align:center;">Series Type</td>\
+					  <td style = "width:30%;"><select id="apExposure-{{{source}}" data-native-menu="false" onchange="itemChange(this)"></select></td>\
+					  <td style = "width:10%;text-align:center;">Color</td>\
+					  <td style = "width:15%;">\
+              <input class="apColorButton" id="apColor-{{source}}" type="button" onclick="GetColor(this)" style="background-color:rgb(248, 249, 247);opacity:1;">\
+						<div id="myColorPicker-apColor-{{source}}"> </div>\
+					  </td>\
+				  </tr>\
+					<tr>\
+					  <td>Y-Axis</td>\
+					  <td><select id="apAssign-{{source}}" data-native-menu="false"><option value="0001" onchange="itemChange(this)">1</option><option value="2">2</option><option value="3">3</option></select></td>\
+					  <td></td>\
+					  <td></td>\
+					  <td></td>\
+					  <td>\
+						<div class="tooltip">\
+						  <button id="del_Item-{{Source}}" class="FctButton ui-mini ui-btn-inline" name="btn-store-streams" type="button"  onclick="deleteItem(this)"><img src="icons/ws/jquery_delete.svg"></button>\
+						  <span class="tooltiptext">remove Item</span>\
+						</div>\
+					  </td>\
+					</tr>\
+			  </table>\
+      </div>'
+
+
+
 
 //****************************************
-function GetSeriesItems()
+function deleteItem(that)
 //****************************************
 {
-	var f = new XMLHttpRequest();
-    f.open("GET", "apps/series_items.json", false);
-    f.onreadystatechange = function ()
-    {
-        if(f.readyState === 4)
-        {
-            if(f.status === 200 || f.status == 0)
-            {
-                var res= f.responseText;
-                //alert(res);
-				mySerieItems = JSON.parse(res);
-            }
-        }
-    }
-    f.send(null);
 }
+
+//****************************************
+function itemChange(that)
+//****************************************
+{
+  myActType=that.id.split("-")[0]
+  myActId=that.id.split("-")[1]
+  myItems[myActId][myActType] = that.value
+}
+
+//****************************************
+function changedYAxis(that)
+//****************************************
+{
+	myActType=that.id.split("-")[0]
+  myActId=that.id.split("-")[1]
+  myYAxis[myActId][myActType] = that.value
+}
+
+//****************************************
+function deleteYAxis(button)
+//****************************************
+{
+	myActId=button.id.split("-")[1]
+	delete myYAxis[myActId]
+	YAxis2Screen(myYAxis,false)
+}
+
+//****************************************
+function YAxis2Screen(data, addNew)
+//****************************************
+{
+  my_Y_Axis = ""
+  my_Y_Axis += yAxisHeader
+  yAxisCount = 0
+  newAxisID = 0
+  for (var key in data) {
+    yAxisCount += 1
+    newAxisID = parseInt(key)
+    myYAxis[key]['no']= yAxisCount
+    myHtml2append = yAxisHtml.split("{id}").join(key)
+    myHtml2append = myHtml2append.split("{NO}").join(yAxisCount)
+    my_Y_Axis += myHtml2append
+    }
+  if (addNew == true)
+    {
+      yAxisCount += 1
+      newAxisID = "00000" + (parseInt(newAxisID)+1)
+      newAxisID = newAxisID.slice(newAxisID.length-4,8);
+      myYAxis[newAxisID] = {}
+      myYAxis[newAxisID]['no']= yAxisCount
+      myYAxis[newAxisID]['apYmin']     = ""
+      myYAxis[newAxisID]['apYmax']     = ""
+      myYAxis[newAxisID]['apYpos']     = "0"
+      myYAxis[newAxisID]['apYtype']    = "linear"
+      myYAxis[newAxisID]['apYunit']    = ""
+      myHtml2append = yAxisHtml.split("{id}").join(newAxisID)
+      myHtml2append = myHtml2append.split("{NO}").join(yAxisCount)
+
+      myHtml2append = myHtml2append.split('data-collapsed="true"').join('data-collapsed="false"')
+      my_Y_Axis += myHtml2append        
+    }
+  my_Y_Axis += '</table>'
+  y_Axis_Rows = $('#y_axis_settings');
+  y_Axis_Rows.html(my_Y_Axis).trigger('create');
+  YValue2Screen()
+}
+
+//****************************************
+function YValue2Screen()
+//****************************************
+{
+  // Object.keys(myYAxis).length  -> Anzahl Achsen
+  for (var key in myYAxis )
+  {
+    myActID = key
+    for (var entry in myYAxis[key])
+      {
+        if (entry != 'no')
+         {
+           myObj = $('#'+entry+'-'+myActID)
+          if (myObj[0].nodeName == "SELECT")
+            {
+              $('#'+entry+'-'+myActID).val(myYAxis[key][entry]).selectmenu('refresh');
+            }
+          else
+            {
+              myObj[0].value = myYAxis[key][entry]
+            }
+         }
+      }
+  }
+}
+//****************************************
+function addYAxis()
+//****************************************
+{
+  YAxis2Screen(myYAxis,true)
+}
+
 
 
 //****************************************
 function SetSelectItem()
 //****************************************
 {
-	$.each(mySerieItems, function(idx, myItem){
-    modeslist += "<option value=\"" + myItem + "\">" +  myItem  + "</option>";
+	if (myTimeSettings.ItemType == "Database")
+  {
+    var myType ="Series"
+  }
+  else
+  {
+    var myType ="ListItems"
+  }
+  myItemList = ""
+  $.each(mySerieItems[myType], function(idx, myItem){
+    myItemList += "<option value=\"" + myItem + "\">" +  myItem  + "</option>";
 	})
-  
-   $('#apItem1').append(modeslist).val('avg').selectmenu('refresh');
+ 
+   $('#apItem-0001').children().remove().end();
+   $('#apItem-0001').append(myItemList).val('avg').selectmenu('refresh');
    
    $.each(myExposures, function(idx, myItem){
     ExposureList += "<option value=\"" + myItem + "\">" +  myItem  + "</option>";
 	})
   
-   $('#apExposure1').append(ExposureList).val('spline').selectmenu('refresh');
+   $('#apExposure-0001').append(ExposureList).val('spline').selectmenu('refresh');
   
 	
 	
@@ -236,12 +441,18 @@ function selectChanged(myObj)
       $('#txtDuration')[0].value = myObj.value;
       break;
     }
+    case 'apDataSource' :
+    {
+      myTimeSettings.ItemType  = myObj.value;
+      SetSelectItem();
+      break;
+    }
   }
  isItem = myObj.id.search("apItem") == 0 ? true : false
  if (isItem)
   {
     myId = myObj.id.replace(/[^0-9]/g,"");
-    myHeadLine = document.getElementById("itemsetting"+myId)
+    myHeadLine = document.getElementById("itemsetting-"+myId)
     myText = myHeadLine.children[0].innerHTML
     myNewText=myText.split(">")[0]+">" + myObj.value
     myNewText += myText.substr(myText.search("<span",189))
@@ -258,9 +469,9 @@ function SetupPage(event, ui)
 {
 	// source was for different menu styles and is not needed any more
 	// can be used to display config menus for more items
-  
+  GetAllSets()
   GetSeriesItems();
-  SetSelectItem();
+  YAxis2Screen(myYAxis,false)
   
   myDate= new Date()
   document.getElementById("StartDate").value= myDate.toJSON().slice(0, 4) +'-01-01'
@@ -280,7 +491,9 @@ function SetupPage(event, ui)
     modeslist += "<option value=\"" + mode + "\">" +  mode  + "</option>";
   })
   for (var i=1; i<6; i++){
-    $('#apMode'+ i).append(modeslist).val('avg').selectmenu('refresh');
+      myId = "00000" + (parseInt(i));
+      myId = myId.slice(myId.length-4,8);
+    $('#apMode-'+ myId).append(modeslist).val('avg').selectmenu('refresh');
   }
   
   // start plot display on button click
@@ -330,7 +543,7 @@ function SetupPage(event, ui)
       var apItems = $('#apItem'+ source).val().toString().split(/,\s*/);
       if (apItems =='')
         apItems = null;
-      var apModes = $('#apMode' + source).val().toString();
+      var apModes = $('#apMode-' + source).val().toString();
       var apCounts = $('#apCount' + source).val().toString().split(/,\s*/);
       var apExposures = $('#apExposure' + source).val().toString().split(/,\s*/);
       var apColors = $('#apColor' + source).val().toString().split(/,\s*/);
@@ -446,3 +659,144 @@ function SetupPage(event, ui)
 };
 
 
+
+//************************************************************
+// Some helper function
+//************************************************************
+
+//*****************************************
+function getCheckRadio(myRadioName)
+//*****************************************
+{
+var radios = document.getElementsByName(myRadioName);
+
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    var retVal = radios[i].id
+    // only one radio can be logically checked, don't check the rest
+    break;
+  }
+}
+return retVal
+}
+
+//*****************************************
+function setCheckRadio(myRadioName, radioID)
+//*****************************************
+{
+  var radios = document.getElementsByName(myRadioName);
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].id == radioID) {
+      $("#"+radios[i].id).click()
+      $("#"+radios[i].id).click()
+      // only one radio can be logically checked, don't check the rest
+      break;
+    }
+  }
+}
+
+//*****************************************
+function SaveSetAs()
+//*****************************************
+{
+  console.log("saveSetAs")
+}
+
+//*****************************************
+function SaveSet()
+//*****************************************
+{
+  console.log("saveSet")
+}
+
+//************************************************************
+// PHP Function
+//************************************************************
+
+//************************************************************
+function storeSet(myFileName)
+//************************************************************
+{
+  values2Save = {
+                  "TimeSettings" : myTimeSettings,
+                  "yAxis"        : myYAxis,
+                  "Items"        : myItems,
+                  "chartOptions" : myChartOptions,
+                  "Stackings"    : myStackings,
+                  "Stacks"       : myStacks
+                }
+  $.ajax({
+      url: "apps/app_analyzer_comfort.php",
+      type: "GET",
+         data: {
+              command : 'store_set_settings',
+              filename : myFileName,
+              data : JSON.stringify(values2Save)
+             },
+      contentType: "application/json; charset=utf-8",
+      success: function (response) {
+        console.log('Status of storing :'+ response );
+      },
+      error: function () {
+          console.log("Error - while storing settings")
+
+      }
+   });
+}
+
+//****************************************
+function GetSeriesItems()
+//****************************************
+{
+$.ajax({
+    url: "apps/app_analyzer_comfort.php",
+    type: "GET",
+		   data: {
+			   		command : 'get_set_series_items'
+			   	 },
+    contentType: "application/json; charset=utf-8",
+    success: function (response) {
+    	console.log('Status of storing :'+ response );
+      mySerieItems = JSON.parse(response);
+      SetSelectItem();
+    },
+    error: function () {
+        console.log("Error - while gettins series")
+
+    }
+ });	
+
+
+}
+
+//************************************************************
+function GetAllSets()
+//************************************************************
+{
+$.ajax({
+    url: "apps/app_analyzer_comfort.php",
+    type: "GET",
+		   data: {
+			   		command : 'getFileArray',
+            suffix  : '.pac'
+			   	 },
+    contentType: "application/json; charset=utf-8",
+    success: function (response) {
+    	console.log('Status of storing :'+ response );
+      setlistJson = JSON.parse(response)
+      mySetList = ""
+      $.each(setlistJson, function(idx, mySet){
+        mySet = mySet.split('.')[0]
+        mySetList += "<option value=\"" + mySet + "\">" +  mySet  + "</option>";
+      })
+     
+       $('#apLoadSettings').children().remove().end();
+       $('#apLoadSettings').append(mySetList).val('avg').selectmenu('refresh');
+    },
+    error: function () {
+        console.log("Error - while storing settings")
+
+    }
+ });
+}
---
+++ app_analyzer_comfort.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * -----------------------------------------------------------------------------
+ * @package     smartVISU - App Plot Analyzer Comfort
+ * @author      Andre Kohler
+ * @copyright   2022
+ * @license     GPL [http://www.gnu.de]
+ * -----------------------------------------------------------------------------
+ */
+
+
+require_once '../lib/includes.php';
+
+
+
+// ************************************************
+// function to get all stored set in pages/<your_pages>
+// ************************************************
+function getFileArray($suffix) {
+    $dir = const_path.'pages/'.config_pages;
+    $dirlist = dir($dir);
+		while (($item = $dirlist->read()) !== false) {
+			if (substr($item, 0, 1) != '.') {
+				if (substr($item, -strlen($suffix)) == $suffix) {
+					$id = str_replace('/', '-', $dir . '/' . $item);
+					$id = str_replace('.', '-', $id);
+					$ret[] =  $item;
+				}
+			}			
+		}
+		$dirlist->close();
+		return (json_encode($ret));
+	}
+
+// ************************************************
+// function to get the path of the active pages
+// ************************************************
+
+function get_page_path($value)
+{
+	$myValue = config_pages;
+	return ($myValue);
+}
+
+// ************************************************
+// function to get the settings for the Sets
+// ************************************************
+
+function get_set_settings($value)
+{
+   $myFile = file_get_contents(const_path.'pages/'.config_pages.$value.'.pac');
+   if ($myFile  != false)
+	   {
+  		return ($myFile);
+	   }
+  else
+  {
+	  //$myFile="{'const_path':'".const_path."','config_pages':'".config_pages."'}";
+	  $myFile='{}';
+  }
+  return ($myFile);
+}
+
+// ************************************************
+// function to get the series and lists from backend
+// ************************************************
+
+function get_set_series_items($value)
+{
+   $myFile = file_get_contents(const_path.'pages/'.config_pages.'series_items.json');
+   if ($myFile  != false)
+	   {
+  		return ($myFile);
+	   }
+  else
+  {
+	  //$myFile="{'const_path':'".const_path."','config_pages':'".config_pages."'}";
+	  $myFile='{}';
+  }
+  return ($myFile);
+}
+
+// ************************************************
+// function to store the settings for the Sets
+// ************************************************
+
+function store_set_settings($value)
+{
+  $content = $value["data"];
+  $file    = $value["filename"];
+  file_put_contents(const_path.'pages/'.config_pages.$file.'.pac', $content);
+	return ('OK');
+}
+
+
+// ************************************************
+// main
+// ************************************************
+
+$command = $_GET["command"];
+
+switch ($command)
+{
+    case 'get_page_path':
+        $ret = get_page_path($_GET);
+        break;
+    case 'get_set_settings':
+        $ret = get_cam_settings($_GET);
+        break;        
+    case 'store_set_settings':
+        $ret = store_cam_settings($_GET);
+        break;
+    case 'getFileArray':
+        $ret = getFileArray($_GET['suffix']);
+        break;
+    case 'get_set_series_items':
+        $ret = get_set_series_items($_GET);
+        break;
+}
+echo $ret;
+?>
+
